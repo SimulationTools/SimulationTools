4ed78ef7545a703fa261b9d4346fecf71af09ae1
Ian Hinder <ian.hinder@aei.mpg.de>
2009-12-04 09:55:50 +0100

    Grids.m: Add functions for plotting Carpet grid output

--- Grids.m
+++ Grids.m
@@ -1,9 +1,13 @@
 
-BeginPackage["Grids`", {"RunFiles`"}];
+BeginPackage["Grids`", {"RunFiles`", "NR`"}];
 
 AnimateGrids::usage = "AnimateGrids[runname] creates an animation of the grid structure of a run";
 LoadGrids;
 PlotGrids;
+PlotGrid;
+GridsBoxes;
+ReadCarpetGrids;
+PlotCarpetGrids2D;
 
 Begin["`Private`"];
 
@@ -35,9 +39,13 @@ rotateLine[{it_, t_, rl_, c_, x1_, y1_, z1_, x2_, y2_, z2_}] :=
 simBoxes[grids_, t_] :=
  Module[{grids2, boxes, boxes2, lines2},
   grids2 = gridAtTime[grids, t];
-  lines2 = Flatten[Map[rotateLine[reflectLineZ[#]] &, grids2], 1];
+(*  lines2 = Flatten[Map[rotateLine[reflectLineZ[#]] &, grids2], 1]; *)
+  lines2 = grids2;
   boxes = Map[boxFromLine, lines2];
   boxes2 = Flatten[boxes, 1]];
+
+GridsBoxes[grids_, t_] :=
+  simBoxes[grids, t];
 
 PlotGrids[grids_, t_, R_] :=
  Graphics3D[{Blue,EdgeForm[Thin],Opacity[0.1], simBoxes[grids, t]}, 
@@ -54,6 +62,98 @@ AnimateGrids[runName_String, R_?NumberQ, dt_?NumberQ, fps_?NumberQ] :=
   Animate[PlotGrids[grids, t, R], {t, tMin, tMax, dt}, 
    ControlPlacement -> Bottom, AnimationRate -> fps, 
    AnimationRunning -> False, AnimationRepetitions -> 1]];
+
+ReadCarpetGridsFromFile[fileName_] :=
+ Module[{lines, gridLines, gridLines1, iterations, readIteration, its,
+    itNos, itVals, itPairs, itPairs1, itPairs2, trToVec},
+
+  trToVec[str_] :=
+    ToExpression /@ StringSplit[str, ","];
+
+  readIteration[itLines_] :=
+   Module[{readLine},
+    readLine[line_] :=
+     Module[{vecs},
+      vecs = 
+       StringCases[line[[5]], 
+         "([" ~~ Shortest[x1__] ~~ "]:[" ~~ Shortest[x2__] ~~ 
+           "]" :> {x1, x2}][[1]];
+      trToVec /@ vecs];
+    readLine /@ 
+     Select[itLines, 
+      Length[#] == 5 && StringQ[#[[5]]] && 
+        StringTake[#[[5]], 1] == "(" &]];
+  
+  lines = ReadColumnFile[fileName];
+
+(*  lines = ReadColumnFile[run, fileName];*)
+  its = SplitBy[lines, Length[#] >= 1 && #[[1]] == "iteration" &];
+  itNos = Take[its, {1, Length[its], 2}];
+  itVals = Take[its, {2, Length[its], 2}];
+  itPairs1 = MapThread[List, {Last[#[[1]]] & /@ itNos, itVals}];
+  itPairs = SplitBy[itPairs1, #[[1]] &];
+  (* Select only map 0 *)
+  
+  itPairs2 = Map[First, itPairs];
+  Map[{#[[1]], readIteration[#[[2]]]} &, itPairs2]
+  ];
+
+mergeFiles[files_List] :=
+  Module[{file1, fileEndIndex, rest, rest0, restIndex, truncated},
+    If[Length[files] == 0, Return[{}]];
+    If[Length[files] == 1, Return[First[files]]];
+
+    file1 = First[files];
+
+    (* The first column of the files will be taken to be an
+    index. Usually this will be an interation number or a coordinate
+    time. *)
+    fileEndIndex = First[Last[file1]];
+    rest = mergeFiles[Rest[files]];
+
+    rest0 = First[rest];
+    restIndex = First[rest0];
+
+    (* The most common case: no overlap *)
+    If[restIndex > fileEndIndex,
+      Return[Join[file1, rest]]];
+
+    (* We have some overlap *)
+    truncated = Select[file1, (First[#] < restIndex) &];
+    Return[Join[truncated, rest]];
+  ];
+
+ReadCarpetGrids[run_, fileName_:"carpet-grid.asc"] :=
+ Module[{fileNames, files},
+  fileNames = FindRunFile[run, fileName];
+  If[files === {}, Throw["No file " <> fileName <> " found in simulation " <> run]];
+  files = Map[ReadCarpetGridsFromFile, fileNames];
+  mergeFiles[files]];
+
+plotBox2D[{v1_, v2_}] :=
+ {Opacity[0], 
+  EdgeForm[Thin], {Rectangle[Take[v1, 2], Take[v2, 2]]}}
+
+plotIteration2D[{it_, boxes_}] :=
+ {Map[plotBox2D, boxes]}
+
+PlotCarpetGrids2D[grids_List, dtFine_?NumberQ, t_?NumberQ] :=
+  Module[{},
+   plotIteration2D[Last[Select[grids, #[[1]] <= t/dtFine &]]]]
+
+PlotCarpetGrids2D[run_String] :=
+  Module[{grids, dt, tRange},
+  grids = ReadCarpetGrids[run];
+  dt = ReadFineTimeStep[run];
+  tRange = ReadTimeRange[run];
+  Manipulate[Graphics[PlotCarpetGrids2D[grids, dt, t]], {{t, tRange[[1]], "t"}, tRange[[1]], tRange[[2]]}]];
+
+PlotCarpetGrids2D[run_String, t_?NumberQ] :=
+  Module[{grids, dt, tRange},
+  grids = ReadCarpetGrids[run];
+  dt = ReadFineTimeStep[run];
+  tRange = ReadTimeRange[run];
+  Graphics[PlotCarpetGrids2D[grids, dt, t]]];
 
 End[];
 
