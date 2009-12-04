5a1bab9d1141c86113d2ed2c4c999a74d05ea6c5
Ian Hinder <ian.hinder@aei.mpg.de>
2009-12-04 09:56:27 +0100

    Profile.m: Add profiling of function call counts

--- Profile.m
+++ Profile.m
@@ -3,6 +3,7 @@ BeginPackage["Profile`"];
 
 Profile;
 ProfileTime;
+ProfileCount;
 
 Begin["`Private`"];
 
@@ -13,7 +14,9 @@ Profile[name_, code_] :=
     name2 = Evaluate[name];
     {time, result} = AbsoluteTiming[ReleaseHold[code]];
     If[Head[ProfileTime[name2]] === ProfileTime, ProfileTime[name2] = 0.0];
+    If[Head[ProfileCount[name2]] === ProfileCount, ProfileCount[name2] = 0];
     ProfileTime[name2] += time;
+    ProfileCount[name2] += 1;
     result];
 
 End[];
