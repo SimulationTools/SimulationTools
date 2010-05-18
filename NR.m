a9dfa21c20201d2d942b615bef0d0fa4e8260c1f
Ian Hinder <ian.hinder@aei.mpg.de>
2010-05-18 16:47:21 +0200

    Move parameter file functions from NR.m to a new package Parameters.m

--- NR.m
+++ NR.m
@@ -2,7 +2,7 @@
 (* A package for dealing with numerical relativity data *)
 
 BeginPackage["NR`", {"DataTable`", "Memo`", "RunFiles`", "Timers`", 
-  "Horizons`"}];
+  "Horizons`", "Parameters`"}];
 
 ReadPsi4::usage = "ReadPsi4[run, l, m, r] returns a DataTable of the l,m mode of Psi4 at radius r from run.";
 ReadMultipolePsi4::usage = "ReadMultipolePsi4[run, l, m, r] returns a DataTable of the l,m mode of Psi4 at radius r from run using the output of the Multipole thorn.";
@@ -63,8 +63,6 @@ ReadRunSpeed;
 ReadPsi4Radii;
 LocateMaximum;
 LocateMaximumPoint;
-LookupParameter;
-FindParameters;
 GridSpacingOnLevel;
 BoxRadiiOnLevel;
 RefinementLevels;
@@ -100,7 +98,6 @@ ReadPunctureADMMasses2;
 CostAnalysis;
 PresentationCostAnalysis;
 PercentageDifference;
-ParseParameterFile;
 
 ReadHamiltonianConstraintNorm;
 
@@ -970,87 +967,7 @@ LocateMaximum[d_DataTable] :=
   tMax2 = 
    t /. FindMaximum[{fn[t], {t > tMax - 50, t < tMax + 50}}, {t, 
        tMax}][[2]];
-  Return[tMax2]]
-
-(* Parameter file parsing *)
-
-unbreakBrokenStrings[lines2_List] :=
-  Module[{oddQuotes, positions, pairs, lines},
-    oddQuotes[s_String] := OddQ[Length[StringCases[s, "\""]]];
-    lines = lines2;
-    positions = First/@Position[lines, _?oddQuotes, {1}];
-    pairs = Partition[positions, 2];
-
-    Scan[Function[pair,
-      lines[[pair[[1]] ;; pair[[2]]]] =
-        StringJoin[Riffle[lines[[pair[[1]] ;; pair[[2]]]], " "]];
-      lines = Drop[lines, {pair[[1]]+1, pair[[2]]}]],
-
-      Reverse[pairs]];
-    Return[lines]];
-
-DefineMemoFunction[ParseParameterFile[from_String],
- Module[{lines, parseLine, thorns, param, val, removeWhiteSpace, fileName, fileNames},
-  (* Is "from" a full parameter file name? *)
-  If[StringMatchQ[from, __ ~~ ".par"],
-    fileNames = {from},
-(*    fileNames = FindRunFile[from, from <> ".par"];*)
-    (* Is "from" a run name? *)
-    fileNames = FindRunFilesFromPattern[from, "*.par"];
-    If[Length[fileNames] == 0,
-      fileNames = FindRunFile[from, from <> "-1.par"];
-      If[Length[fileNames] == 0,
-        Throw["Cannot find parameter file " <> ToString[from]]],
-      fileNames = FindRunFile[from, fileNames[[1]]]]];
-  fileName = First[fileNames];
-  lines = ReadList[fileName, String];
-  removeWhiteSpace[t_] := 
-   StringReplace[
-    t, (StartOfString ~~ Whitespace) | (Whitespace ~~ EndOfString) :> 
-     ""];
-
-  lines = unbreakBrokenStrings[lines];
-
-  parseLine[s_] :=
-   If[StringMatchQ[s, RegularExpression["[ \t]*#.*"]],
-    Comment[s],
-    If[StringMatchQ[
-      s, (Whitespace | StartOfString) ~~ 
-       "ActiveThorns" ~~ (Whitespace | "=") ~~ __, IgnoreCase -> True],
-     ActiveThorns[
-      StringSplit[
-       StringCases[s, "\"" ~~ thorns__ ~~ "\"" -> thorns][[1]]]],
-     If[StringMatchQ[s, RegularExpression[".*::.*=.*"]],
-      ParameterSetting[
-       ToLowerCase[
-        removeWhiteSpace[
-         StringCases[s, param__ ~~ "=" ~~ val__ -> param][[1]]]], 
-       removeWhiteSpace[
-        StringCases[s, param__ ~~ "=" ~~ val__ -> val][[1]]]],
-      Throw["Unrecognized line in parameter file: " <> s]]]];
-  Map[parseLine, lines]
-  ]];
-
-LookupParameter[parFile_List, name_, default_:None] :=
- Module[{l},
-  l = Cases[parFile, ParameterSetting[ToLowerCase[name], x_] -> x];
-  If[l === {} && default =!= None, Return[default]];
-  If[Length[l] == 0, Throw["Parameter " <> name <> " not found"]];
-  First[l]];
-
-LookupParameter[from_String, name_, default_:None] :=
-  Module[{},
-    (* Assume the parameter file is named after the run *)
-    LookupParameter[ParseParameterFile[from], name, default]
-  ];
-
-FindParameters[parFile_String, pattern_] :=
-  FindParameters[ParseParameterFile[parFile], pattern];
-
-FindParameters[parFile_List, pattern_] :=
-  Module[{parameters},
-    parameters = Cases[parFile, ParameterSetting[name_,value_] -> name];
-    Select[parameters, StringMatchQ[#, pattern, IgnoreCase->True] &]];
+  Return[tMax2]];
 
 GridSpacingOnLevel[runName_, l_] :=
   Module[{h0},
