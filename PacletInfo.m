41daad4398de76395d699247bada4c0581cf765b
Barry Wardell <barry.wardell@gmail.com>
2011-03-15 01:22:48 +0100

    Update documentation to use built-in Mathematica system.

---
+++ PacletInfo.m
@@ -0,0 +1,18 @@
+(* Paclet Info File *)
+
+(* created 2011/03/14*)
+
+Paclet[
+    Name -> "nrmma",
+    Version -> "0.0.1",
+    MathematicaVersion -> "6+",
+    Creator -> "Ian Hinder and Barry Wardell",
+    Extensions -> 
+        {
+            {"Documentation", Resources -> 
+                {"Guides/NRMMA"}
+            , Language -> "English"}
+        }
+]
+
+
