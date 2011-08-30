6f869bd3a4328326dbb67e1ac83045302777b4bc
Ian Hinder <ian.hinder@aei.mpg.de>
2011-08-30 16:10:00 +0200

    NRExport.m: Take second entry of ReadIsolatedHorizonSpin table
    This avoids getting poison from the initial data (temporary hack)

--- NRExport.m
+++ NRExport.m
@@ -284,7 +284,7 @@ runMetadata[run_, mass_, ecc_, tJunk_] :=
         M^2, {i, 0, 1}, {d, 1, 3}],
    Sequence @@ Flatten@Table[
       "initial-bh-spin" <> ToString[i+1] <> coord[d] ->
-       First@DepVar@ReadIsolatedHorizonSpin[run, i, d]/
+       DepVar[ReadIsolatedHorizonSpin[run, i, d]][[2]]/ (* Poison in the first entry of q1D11.8S0.3s0b *)
         M^2, {i, 0, 1}, {d, 1, 3}],
    "data-type" -> "NR"
    }
