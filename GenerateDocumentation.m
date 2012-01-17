760d24a07cda92e95d0d9d973be5fded1772c8e1
Ian Hinder <ian.hinder@aei.mpg.de>
2012-01-17 15:07:01 +0100

    Add script and front-end notebook for generating documentation
    This requires the ApplicationMaker package.  The new documentation coexists with the existing Workbench documentation.  Symbol reference pages are generated for all symbols with a usage message.

---
+++ GenerateDocumentation.m
@@ -0,0 +1,63 @@
+<< nrmma`;
+
+<< ApplicationMaker`;
+
+packages =
+ {"Ascii1D",
+  "Ascii",
+  "BHCoordinates",
+  "CarpetHDF5",
+  "CoordinateTransformations",
+  "Convergence",
+  "DataAnalysis",
+  "DataRegion",
+  "DataTable",
+  "FieldLines",
+  "Grids",
+  "Horizons",
+  "Kicks",
+  "Memo",
+  "NR",
+  "NRExport",
+  "nrmmaVersion",
+  "Parameters",
+  "Plotting",
+  "Profile",
+  "ReadHDF5",
+  "RunFiles",
+  "SimView",
+  "SimViewRRMHD",
+  "SystemStatistics",
+  "Tracks"};
+
+documentedSymbols[package_String] :=
+  Select[Names[package<>"`*"], StringQ[ToExpression[#<>"::usage"]] &];
+
+packageSymbols = Map[# -> documentedSymbols[#] &, packages];
+
+workbenchSymbols =
+  ToString/@{AsciiDataOfIndex,
+   AsciiTimeOfIndex,
+   ConvergenceMultiplier,
+   ConvergenceRate,
+   FindParameters,
+   ListLinePlotWithLegend,
+   LookupParameter,
+   PresentationListLinePlot,
+   ReadCPUHours,
+   ReadCarpetASCII1D,
+   ReadCores,
+   ReadMemory,
+   ReadRunSpeed,
+   ReadSwap,
+   ReadWalltime,
+   RunDirectory,
+   SimView};
+
+docPackage[package_ -> symbols_] :=
+  Map[CreateReferencePages["nrmma", FileNameJoin[{$UserBaseDirectory, "Applications"}],
+                           package, #, ReferenceTemplate -> False] &, Complement[Take[symbols,Min[Infinity,Length[symbols]]], workbenchSymbols]];
+
+Scan[docPackage, packageSymbols];
+
+BuildApplication["nrmma", CreatePacletInfo -> False];
