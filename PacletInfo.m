4035ed696768087315113b7a99a473bc497d5f73
Ian Hinder <ian.hinder@aei.mpg.de>
2012-01-18 02:05:23 +0100

    PacletInfo.m: Add contexts
    This enables the link from the ?symbol output to the documentation page.

--- PacletInfo.m
+++ PacletInfo.m
@@ -3,16 +3,45 @@
 (* created 2011/03/14*)
 
 Paclet[
-    Name -> "nrmma",
-    Version -> "0.0.1",
-    MathematicaVersion -> "6+",
-    Creator -> "Ian Hinder and Barry Wardell",
-    Extensions -> 
-        {
-            {"Documentation", Resources -> 
-                {"Guides/NRMMA"}
-            , Language -> "English"}
-        }
-]
+  Name -> "nrmma",
+  Version -> "0.0.1",
+  MathematicaVersion -> "6+",
+  Creator -> "Ian Hinder and Barry Wardell",
+  Extensions -> 
+  {
+    { "Kernel",
+      "Context" -> {
+        "Ascii1D`",
+        "Ascii`",
+        "BHCoordinates`",
+        "CarpetHDF5`",
+        "CoordinateTransformations`",
+        "Convergence`",
+        "DataAnalysis`",
+        "DataRegion`",
+        "DataTable`",
+        "FieldLines`",
+        "Grids`",
+        "Horizons`",
+        "Kicks`",
+        "Memo`",
+        "NR`",
+        "NRExport`",
+        "nrmmaVersion`",
+        "Parameters`",
+        "Plotting`",
+        "Profile`",
+        "ReadHDF5`",
+        "RunFiles`",
+        "SimView`",
+        "SimViewRRMHD`",
+        "SystemStatistics`",
+        "Tracks`"}
+    },
 
-
+    {"Documentation", 
+     Resources -> 
+     {"Guides/NRMMA"},
+     Language -> "English"}
+  }
+      ]
