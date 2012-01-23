edfc3059a7684460dc3b779b1db5e96cb6577035
Barry Wardell <barry.wardell@gmail.com>
2012-01-23 14:28:29 +0000

    Kicks: Fix usage message for KickVector.

--- Kicks.m
+++ Kicks.m
@@ -3,7 +3,7 @@
 BeginPackage["Kicks`", {"NR`", "DataTable`", "Memo`", "Profile`"}];
 
 Kick::usage = "Kick[run, dir, r, lMax] computes the kick component in km/s from the multipolar decomposition of Psi4 on a sphere at radius r in direction dir (an integer from 1 to 3) using modes up to l = lMax";
-KickVector::usage = "Kick[run, r, lMax] computes the vector kick in km/s from the multipolar decomposition of Psi4 on a sphere at radius r using modes up to l = lMax"
+KickVector::usage = "KickVector[run, r, lMax] computes the vector kick in km/s from the multipolar decomposition of Psi4 on a sphere at radius r using modes up to l = lMax"
 LinearMomentumRadiated::usage = "LinearMomentumRadiated[run, dir, r, lMax] computes the linear momentum radiated as a function of time from the multipolar decomposition of Psi4 on a sphere at radius r in direction dir (an integer from 1 to 3) using modes up to l = lMax.  It returns a DataTable.  The result is an integral of the result of LinearMomentumFlux with an initial boundary condition of zero.";
 LinearMomentumFlux::usage = "LinearMomentumFlux[run, dir, r, lMax] computes the linear momentum flux as a function of time from the multipolar decomposition of Psi4 on a sphere at radius r in direction dir (an integer from 1 to 3) using modes up to l = lMax.  It returns a DataTable.  The result is the derivative of the result of LinearMomentumRadiated.";
 AngularMomentumFlux::usage = "WARNING: THIS FUNCTION IS IN DEVELOPMENT AND HAS NOT BEEN TESTED. AngularMomentumFlux[run, dir, r, lMax] computes the angular momentum flux as a function of time from the multipolar decomposition of Psi4 on a sphere at radius r in direction dir (an integer from 1 to 3) using modes up to l = lMax.  It returns a DataTable.";
