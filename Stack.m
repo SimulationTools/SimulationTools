d64adfa930062e7a96cef638b2cb8a96f3bc4773
Ian Hinder <ian.hinder@aei.mpg.de>
2012-02-22 13:24:47 +0100

    Stack.m: Add stack-handling package

---
+++ Stack.m
@@ -0,0 +1,39 @@
+
+BeginPackage["Stack`"];
+
+WithStackFrame::usage = "WithStackFrame[sf,expr] evaluates expr and adds the stack frame sf to the evaluation stack.";
+ShowStack::usage = "ShowStack[] returns a representation of the evaluation stack in a form suitable for display.";
+CurrentStack::usage = "CurrentStack[] returns the current evaluation stack.";
+ClearStack::usage = "ClearStack[] clears the current evaluation stack.";
+
+Begin["`Private`"];
+
+(* A stack is a list of stack frames.  A stack frame is an expression
+   of the form Hold[fn[args...]]. *)
+
+stack = {};
+
+SetAttributes[WithStackFrame, HoldAll];
+WithStackFrame[sf_, expr_] := 
+  Module[{r},
+   AppendTo[stack, sf];
+   r = expr;
+   stack = Drop[stack,-1];
+   r];
+
+(* Options[ShowStack] = {"IncludeArguments" -> False}; *)
+ShowStack[s_:Automatic] :=
+  Module[
+    {},
+    Print[StringJoin[Riffle[Map["in "<>ToString[#] &, Reverse@If[s===Automatic, stack, s]/.Hold->HoldForm],"\n"]]];
+        ];
+
+CurrentStack[] :=
+  stack;
+
+ClearStack[] :=
+  stack = {};
+
+End[];
+
+EndPackage[];
