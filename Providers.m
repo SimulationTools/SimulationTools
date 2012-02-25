2460da0c88fe35a6e54ed44954d2cd0fba041503
Ian Hinder <ian.hinder@aei.mpg.de>
2012-02-25 18:44:24 +0100

    Providers.m: Add ProviderPreferences feature
    This allows the user to specify the order in which different providers are preferred in the case that data from multiple providers is available.

--- Providers.m
+++ Providers.m
@@ -2,41 +2,48 @@
 BeginPackage["Providers`", {"Error`"}];
 
 CallProvidedFunction;
+ProviderPreferences::usage = "ProviderPreferences[f] gives a list of providers for feature f which determines the preference for data from these providers.  This can be reset by the user, either globally or within a Block.";
 
 Begin["`Private`"];
 
+ProviderPreferences[_] := {};
+
 CallProvidedFunction[base_String, fn_String, args_List, method_:Automatic] :=
   Module[
-    {providerHaveFns, providers, provider, have, posns, f},
+    {providerHaveFns, providers, provider, have, posns, f, numProviders, providersWithData},
 
     (* Print["CallProvidedFunction["<>StringJoin[Riffle[ToString[#,InputForm]& /@ {base, fn, args, method},","]]<>"]"]; *)
 
     providerHaveFns = Names["*`"<>base<>"`HaveData"];
     providers = Map[First[StringSplit[#,"`"]] &, providerHaveFns];
 
-    provider =
-    If[method === Automatic,
-       (* providers = ToExpression/@providerFns; *)
-       have = Map[ToExpression[#]@@args &, providerHaveFns];
-       Scan[If[# =!= True && # =!= False, Error["CallProvidedFunction: Unknown result for HaveData in "<>ToString[have,InputForm]]] &, have];
-       (* Print["providers = ", providers]; *)
-       (* Print["have = ", have]; *)
-       posns = Position[have, True];
-       If[Length[posns] > 1,
-          Error["Multiple data sources for "<>base<>" ("<>
-                ToString[providers,InputForm]<>
-                "): Please select one using Method -> source, where source is one of " <>
-                ToString[providers]]];
+    (* providers = ToExpression/@providerFns; *)
+    have = Map[ToExpression[#]@@args &, providerHaveFns];
+    Scan[If[# =!= True && # =!= False, Error["CallProvidedFunction: Unknown result for HaveData in "<>ToString[have,InputForm]]] &, have];
+    (* Print["providers = ", providers]; *)
+    (* Print["have = ", have]; *)
+    posns = Position[have, True];
+    numProviders = Length[posns];
 
-       If[Length[posns] == 0,
-          CallProvidedFunction::nodata = "No data for `1`";
-          ErrorMessage[CallProvidedFunction::nodata, base<>"`"<>fn<>"["<>
-                       StringJoin[Riffle[ToString[#,InputForm]&/@args,","]]<>"]"]];
-       providers[[posns[[1,1]]]],
-       method];
+    provider = Which[
+      numProviders === 1,
+      providers[[posns[[1,1]]]],
 
-    (* Print["provider = ", provider]; *)
+      numProviders === 0,
+      CallProvidedFunction::nodata = "No data for `1`";
+      ErrorMessage[CallProvidedFunction::nodata, base<>"`"<>fn<>"["<>
+                   StringJoin[Riffle[ToString[#,InputForm]&/@args,","]]<>"]"],
 
+      numProviders > 1,
+      providersWithData = Extract[providers,posns];
+      Module[
+        {choices = Select[ProviderPreferences[base], MemberQ[providersWithData,#]&]},
+        If[choices === {},
+           CallProvidedFunction::mulsrc = "Multiple data sources for `1` (`2`) and ProviderPreferences[`3`] does not specify which is preferred.  Please set ProviderPreferences[`3`] to a list of providers for `1` indicating the order in which they are preferred.";
+           ErrorMessage[CallProvidedFunction::mulsrc, base, StringJoin[Riffle[providersWithData,", "]], ToString[base,InputForm]],
+           (* else *)
+           First[choices]]]];
+    
     If[!MemberQ[providers, provider],
        Error[base<>": Provider "<>ToString[provider,InputForm]<>" not found"]];
 
