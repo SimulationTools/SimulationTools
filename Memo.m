42f95151f282e7b3c956839374a2fd695e17166c
Ian Hinder <ian.hinder@aei.mpg.de>
2012-02-29 18:35:02 +0100

    Memo.m: Rewrite memoisation
    Memoised values are now not lost when DefineMemoFunction is called again.  It is now possible to have multiple memoised functions with the same name and different arguments.

--- Memo.m
+++ Memo.m
@@ -2,28 +2,21 @@
 
 BeginPackage["Memo`"];
 
-Memo::usage = "Memo is a symbol used by the Memo package.  It is not needed by end-users.";
 DefineMemoFunction::usage = "DefineMemoFunction[f[args], body] defines a function f with arguments args and body body such that the result of the function is remembered after the first time it is called.  This is used to cache data that is expensive to read or compute.  See ClearAllMemos for flushing this cache.";
 ClearAllMemos::usage = "ClearAllMemos[] wipes the cache of functions that have been memoised using DefineMemoFunction.";
 
 Begin["`Private`"];
 
+If[!ValueQ[$cache], $cache = {}];
+
 SetAttributes[DefineMemoFunction, HoldAll];
-
-Memo[f_] :=
-   Function[
-       If[Head[DataCache[f, {##}]] === DataCache,
-          DataCache[f, {##}] = Apply[f,{##}]];
-       DataCache[f, {##}]];
-
 DefineMemoFunction[name_[args___], body_] :=
-  Module[{f},
-    Options[f] = Options[name];
-    SetDelayed[f[args], body];
-    name = Memo[f]];
+  Module[
+    {pat},
+    pat : name[args] := (AppendTo[$cache, Hold[pat]]; pat = body)];
 
 ClearAllMemos[] :=
-  Clear[DataCache];
+  ($cache /. Hold -> Unset; $cache = {};)
 
 End[];
 
