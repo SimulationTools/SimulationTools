f8a111775b319d9f57690c9fbf3dcf1bab28f343
Ian Hinder <ian.hinder@aei.mpg.de>
2012-07-12 18:02:59 +0200

    GenerateDocumentation.m: Add HTML export of tutorials
    By default this goes to ~/Sites/nrmma/examples.  You can override this
    by setting $HTMLDestination.

--- Source/GenerateDocumentation.m
+++ Source/GenerateDocumentation.m
@@ -84,3 +84,53 @@ Map[BuildTutorial[FileNameJoin[{Directory[], #}]]&, tutorialSources];
 
 Print["Indexing Documentation"];
 BuildIndex["nrmma"];
+
+(**********************************************************************************************************)
+(* HTML Documentation *)
+(**********************************************************************************************************)
+
+Print["Exporting HTML Documentation"];
+
+Module[
+  {exportNotebook, docDir,dest,tutorials,tutorialHTMLNames,tutorialTitles},
+
+  exportNotebook[dest_String, nbf_String] :=
+  Module[{nb},
+         Print["  Exporting ", FileNameTake[nbf,-1]];
+         nb = NotebookOpen[nbf];
+         Export[dest<>"/" <> 
+                StringReplace[FileNameTake[nbf, -1], ".nb" -> ".html"], nb];
+         NotebookClose[nb]];
+
+  docDir = FileNameJoin[{FileNameDrop[FindFile["nrmma`"], -2], "Documentation"}];
+
+  dest = If[StringQ[$HTMLDestination], $HTMLDestination, "~/Sites/nrmma/examples"];
+
+  tutorials = 
+  Select[FileNames["*", FileNameJoin[{docDir, "English/Tutorials"}], 
+                   Infinity], ! DirectoryQ[#] &];
+
+  Print["Exporting tutorials"];
+
+  Scan[exportNotebook[dest,#] &, tutorials];
+
+  tutorialHTMLNames = 
+  Map[StringReplace[FileNameTake[#, -1], ".nb" -> ".html"] &, 
+      tutorials];
+
+  tutorialTitles = {
+    "DataRegion.html" -> "DataRegion", 
+    "DataTable.html" -> "DataTable", 
+    "GridFunctions.html" -> "Grid functions",
+    "Kicks.html" -> "Kicks", 
+    "NRMMAIntroduction.html" -> "Introduction", 
+    "NumericalRelativity.html" -> "Numerical Relativity"};
+
+  Print["Exporting tutorial index"];
+
+  Export[dest<>"/list.html", 
+         Map["<li><a href = \"examples/" <> # <> 
+             "\">" <> (# /. tutorialTitles) <> "</a></li>" &, 
+             tutorialHTMLNames], "Text"]];
+
+Print["Done"];
