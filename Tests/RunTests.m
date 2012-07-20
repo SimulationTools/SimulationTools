8b87281e41cfe3c361d7b9fae5820af384ecff24
Barry Wardell <barry.wardell@gmail.com>
2012-07-20 10:20:40 +0100

    Update script for running tests from the command line.

--- Tests/RunTests.m
+++ Tests/RunTests.m
@@ -1,8 +1,22 @@
-AppendTo[$Path, "/Applications/WolframWorkbench.app/configuration/org.eclipse.osgi/bundles/13/1/.cp/MathematicaSourceVersioned/Head"];
+AppendTo[$Path, "/Applications/Wolfram Workbench.app/configuration/org.eclipse.osgi/bundles/13/1/.cp/MathematicaSourceVersioned/Head"];
 
 << MUnit`
 << nrmma`
 
-TestRun["DataTable.mt", Loggers -> {VerbosePrintLogger[]}, TestRunTitle -> "DataTable"];
+tests = {
+  "BHCoordinates",
+  "CarpetHDF5",
+  "DataRegion",
+  "DataRegionDeprecated",
+  "DataTable",
+  "DataTableNew",
+  "GridFunctions",
+  "Memo",
+  "NR",
+  "RunFiles",
+  "Waveforms"
+};
+
+(Print["\n"]; TestRun[#<>".mt", Loggers -> {VerbosePrintLogger[]}, TestRunTitle -> #]) & /@ tests;
 
 
