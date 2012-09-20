824bd5be8577b178ec8058f10c9f568aca66d5f0
Barry Wardell <barry.wardell@gmail.com>
2012-09-20 15:27:55 +0100

    Memo: Disable memoisation by default.
    Introduce a $Memoisation variable to control whether it is enabled or disabled.
    The default value is False, unless $NRMMACompatibilityVersion is less than 1.

--- Memo.m
+++ Memo.m
@@ -14,12 +14,25 @@
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 *)
 
-BeginPackage["Memo`"];
+BeginPackage["Memo`",
+ {
+  "Utils`"
+ }
+];
 
 DefineMemoFunction::usage = "DefineMemoFunction[f[args], body] defines a function f with arguments args and body body such that the result of the function is remembered after the first time it is called.  This is used to cache data that is expensive to read or compute.  See ClearAllMemos for flushing this cache.";
 ClearAllMemos::usage = "ClearAllMemos[] wipes the cache of functions that have been memoised using DefineMemoFunction.";
+$Memoisation::usage = "$Memoisation is a variable which controls whether expensive functions are memoised or not.";
 
 Begin["`Private`"];
+
+If[!ValueQ[$Memoisation],
+  If[$NRMMACompatibilityVersion < 1,
+    $Memoisation = True;
+  ,
+    $Memoisation = False;
+  ];
+];
 
 If[!ValueQ[$cache], $cache = {}];
 
@@ -30,7 +43,10 @@ DocumentationBuilder`SymbolDescription["ClearAllMemos"] =
 
 SetAttributes[DefineMemoFunction, HoldAll];
 DefineMemoFunction[name_[args___], body_] :=
-    pat : name[args] := (AppendTo[$cache, Hold[pat]]; pat = body);
+ Module[{},
+  pat : name[args] /; $Memoisation  := (AppendTo[$cache, Hold[pat]]; pat = body);
+  pat : name[args] /; !$Memoisation := body;
+ ]
 
 ClearAllMemos[] :=
   ($cache /. Hold -> Unset; $cache = {};)
