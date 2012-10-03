7549b860cf2db6576f781dedae84b92826aaa92f
Barry Wardell <barry.wardell@gmail.com>
2012-10-03 17:14:57 -0400

    PacletInfo: Update list of contexts.

--- PacletInfo.m
+++ PacletInfo.m
@@ -12,41 +12,67 @@ Paclet[
   {
     { "Kernel",
       "Context" -> {
-        "SimulationTools`Ascii1D`",
+        "SimulationTools`",
         "SimulationTools`Ascii`",
+        "SimulationTools`Ascii1D`",
+      (*"SimulationTools`AsyncCommand`,"*)
         "SimulationTools`BHCoordinates`",
+        "SimulationTools`Binary`",
+        "SimulationTools`BlackHole`",
+      (*"SimulationTools`CactusTimers`",*)
         "SimulationTools`CarpetHDF5`",
-        "SimulationTools`CoordinateTransformations`",
+      (*"SimulationTools`CarpetHDF5Plot`",*)
+        "SimulationTools`CarpetIOHDF5`",
+      (*"SimulationTools`CarpetVTK`",*)
+      (*"SimulationTools`CircularPN`",*)
         "SimulationTools`Convergence`",
+        "SimulationTools`CoordinateTransformations`",
         "SimulationTools`DataAnalysis`",
-        "SimulationTools`SimulationTools`DataRegion`",
-        "SimulationTools`SimulationTools`DataTable`",
+        "SimulationTools`DataRegion`",
+        "SimulationTools`DataRepresentations`",
+        "SimulationTools`DataTable`",
+      (*"SimulationTools`Empty`",*)
+      (*"SimulationTools`Error`",*)
         "SimulationTools`FieldLines`",
+        "SimulationTools`GridFunctions`",
         "SimulationTools`Grids`",
         "SimulationTools`Horizons`",
+        "SimulationTools`IniFile`",
+        "SimulationTools`InitialData`",
         "SimulationTools`Kicks`",
         "SimulationTools`Memo`",
+        "SimulationTools`MessageCatcher`",
         "SimulationTools`MinTracker`",
+        "SimulationTools`Movie`",
         "SimulationTools`MultipoleASCII`",
         "SimulationTools`MultipoleHDF5`",
         "SimulationTools`NR`",
+        "SimulationTools`NRDF`",
         "SimulationTools`NRExport`",
         "SimulationTools`Parameters`",
+        "SimulationTools`Performance`",
         "SimulationTools`Plotting`",
         "SimulationTools`Profile`",
         "SimulationTools`Providers`",
         "SimulationTools`PunctureTracker`",
+      (*"SimulationTools`Quasinormal`",*)
         "SimulationTools`ReadHDF5`",
         "SimulationTools`RunFiles`",
         "SimulationTools`ShiftTracker`",
         "SimulationTools`SimFactory`",
         "SimulationTools`SimView`",
         "SimulationTools`SimViewRRMHD`",
+      (*"SimulationTools`Stack`",*)
+        "SimulationTools`Statistics`",
         "SimulationTools`SystemStatistics`",
+      (*"SimulationTools`Timers`",*)
+        "SimulationTools`Trackers`",
         "SimulationTools`Tracks`",
         "SimulationTools`TwoPunctures`",
         "SimulationTools`Waveforms`",
-        "SimulationTools`YlmDecomp`"}
+        "SimulationTools`Wavelets`",
+        "SimulationTools`YlmDecomp`"
+       }
     },
 
     {"Documentation",
