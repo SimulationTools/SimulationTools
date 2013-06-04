de9c7ac5c884c5adcc887930a4c20632a4633c64
Ian Hinder <ian.hinder@aei.mpg.de>
2013-06-04 18:19:27 +0200

    Utils.m: Add RunSubprocess function

--- Utils.m
+++ Utils.m
@@ -18,7 +18,7 @@ BeginPackage["SimulationTools`Utils`"];
 
 FilterNaNs(*::usage = "FilterNaNs[d] replaces any NaN (Not a Number) values in the DataRegion d with Missing[], which is Mathematica's notation for missing data."*);
 NaNQ(*::usage = "NaNQ[x] returns True if x is a NaN (Not a Number) value and False if it is not.  Mathematica deals strangely with NaN values imported from other programs.  This function was developed for use with the h5mma package for reading HDF5 data."*);
-
+RunSubprocess;
 
 Begin["`Private`"];
 
@@ -44,6 +44,29 @@ SyntaxInformation[NaNQ] =
 
 NaNQ[x_] :=
  Round[x] == -2147483648;
+
+If[$VersionNumber < 9.,
+   CreateTemporary[] :=
+   Module[
+     {name},
+     name = FileNameJoin[{$TemporaryDirectory, IntegerString[RandomInteger[10^50], 16]}];
+     Close[OpenWrite[name]];
+     name]];
+
+RunSubprocess[{cmd_, args___}] :=
+  Module[
+    {stdoutFile, stderrFile, cmdString, retCode, stdout, stderr},
+    stdoutFile = CreateTemporary[];
+    stderrFile = CreateTemporary[];
+    (* TODO: handle quoting *)
+    cmdString = StringJoin[Riffle[{cmd, args}, " "]];
+    (* Run executes the cmdString using a shell *)
+    retCode = Run[cmdString <> ">"<>stdoutFile<>" 2>"<>stderrFile];
+    stdout = ReadList[stdoutFile, String, NullRecords -> True];
+    stderr = ReadList[stderrFile, String, NullRecords -> True];
+    DeleteFile[stdoutFile];
+    DeleteFile[stderrFile];
+    {retCode, stdout, stderr}];
 
 End[];
 EndPackage[];
