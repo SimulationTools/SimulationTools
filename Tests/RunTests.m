7e470021cfde0e0d6086be9d5314b0b452759280
Ian Hinder <ian.hinder@aei.mpg.de>
2014-05-26 18:26:06 +0200

    RunTests.m: Several improvements
    - Set PageWidth to infinity
    - Detect list of test files automatically
    - Print summary of passing and failing packages at end of run

--- Tests/RunTests.m
+++ Tests/RunTests.m
@@ -1,25 +1,17 @@
 AppendTo[$Path, "/Applications/Wolfram Workbench.app/configuration/org.eclipse.osgi/bundles/13/1/.cp/MathematicaSourceVersioned/Head"];
 
+SetOptions["stdout", PageWidth -> Infinity];
+
 << MUnit`
 << SimulationTools`
 
-tests = {
-		"BHCoordinates",
-		"CarpetHDF5",
-		"DataAnalysis",
-		"DataRegion",
-		"DataRegionDeprecated",
-		"DataTable",
-		"DataTableNew",
-		"GridFunctions",
-		"Grids",
-		"Memo",
-		"NR",
-		"RunFiles",
-		"SimView",
-		"Waveforms"
-};
+tests = Map[FileBaseName[#] &, FileNames["*.mt"]];
 
-(Print["\n"]; TestRun[#<>".mt", Loggers -> {VerbosePrintLogger[]}, TestRunTitle -> #]) & /@ tests;
+results = (Print["\n"]; TestRun[#<>".mt", Loggers -> {VerbosePrintLogger[]}, TestRunTitle -> #]) & /@ tests;
+Print[];
 
+pass = Pick[tests, results];
+fail = Pick[tests, Map[Not,results]];
 
+Print["Passing packages: ", pass];
+Print["Failing packages: ", fail];
