SimulationTools/SimulationTools

commit dc786bf42f1ab4d24362cc1e4c94894b9bba56a7

CarpetIOHDF5: Fix detection of HDF5 files from single component runs

CarpetIOHDF5.m
@@ -180,13 +180,16 @@ SimulationTools`CarpetIOHDF5`GridFunctions`ReadTime[file_String, opts:OptionsPat
 (* FindGridFunctions *)
 (****************************************************************)
 
+filePattern=
+  Repeated[("." ~~ (DigitCharacter ..)), {0, 1}] ~~
+  Repeated["." ~~ ("x" | "y" | "z" | "xy" | "xz" | "yz" |
+           ("file_" ~~ (DigitCharacter ..))), {0, 1}] ~~
+  ".h5";
+
 SimulationTools`CarpetIOHDF5`GridFunctions`FindGridFunctions[sim_String] :=
  Module[{pattern, h5Files, leafnames, varnames, 
    var},
-  pattern = 
-   var__ ~~ 
-    "." ~~ ("x" | "y" | "z" | "xy" | "xz" | 
-      "yz" | ("file_" ~~ (DigitCharacter ..))) ~~ ".h5";
+  pattern = var__ ~~ filePattern;
   h5Files = FindSimulationFiles[sim, pattern];
   leafnames = FileNameTake[#, -1] & /@ h5Files;
   varnames = Union[First[StringSplit[#, "."]] & /@ leafnames]];
@@ -198,11 +201,7 @@ SimulationTools`CarpetIOHDF5`GridFunctions`FindGridFunctions[sim_String] :=
 SimulationTools`CarpetIOHDF5`GridFunctions`ReadGridFunctionDimensions[sim_String, varName_String] :=
   Module[{pattern, h5Files, leafnames, dimStrings, 
    dimRules, x},
-  pattern = 
-   varName ~~ 
-   (("." ~~ (DigitCharacter..)) | "") ~~
-    "." ~~ ("x" | "y" | "z" | "xy" | "xz" | 
-      "yz" | ("file_" ~~ (DigitCharacter ..))) ~~ ".h5";
+  pattern = varName ~~ filePattern;
   h5Files = FindSimulationFiles[sim, pattern];
   leafnames = FileNameTake[#, -1] & /@ h5Files;
   dimStrings = StringSplit[#, "."][[-2]] & /@ leafnames;

commit ce7a1e5412af5cb6d2ee32c409d6c85b569e3e3c

CarpetIOHDF5.m: Check for Carpet dataset in FindGridFunctions
This corrects a regression introduced in
dc786bf42f1ab4d24362cc1e4c94894b9bba56a7 but might make this function
slow for large simulations.  In that case, additional functionality will
be required in h5mma to efficiently determine if the dataset is present.

CarpetIOHDF5.m
@@ -199,11 +199,15 @@ filePattern=
            ("file_" ~~ (DigitCharacter ..))), {0, 1}] ~~
   ".h5";
 
+(* Warning: this could be slow for large simulations *)
+carpetIOHDF5FileQ[filename_String] :=
+  MemberQ[ReadHDF5[filename, {"Datasets"}], "/Parameters and Global Attributes/All Parameters"];
+
 SimulationTools`CarpetIOHDF5`GridFunctions`FindGridFunctions[sim_String] :=
  Module[{pattern, h5Files, leafnames, varnames, 
    var},
   pattern = var__ ~~ filePattern;
-  h5Files = FindSimulationFiles[sim, pattern];
+  h5Files = Select[FindSimulationFiles[sim, pattern], carpetIOHDF5FileQ];
   leafnames = FileNameTake[#, -1] & /@ h5Files;
   varnames = Union[First[StringSplit[#, "."]] & /@ leafnames]];
 