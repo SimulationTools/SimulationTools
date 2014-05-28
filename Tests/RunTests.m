32a8374b3e03104ba981cbdd5ef1b1aa24a2fc84
Ian Hinder <ian.hinder@aei.mpg.de>
2014-05-28 10:50:54 +0200

    RunTests.m: Search for Workbench

--- Tests/RunTests.m
+++ Tests/RunTests.m
@@ -1,10 +1,16 @@
-AppendTo[$Path, "/Applications/Wolfram Workbench.app/configuration/org.eclipse.osgi/bundles/13/1/.cp/MathematicaSourceVersioned/Head"];
+
 (* This script should be run from the SimulationTools/Tests directory like this:
 
      math -run '<<RunTests.m' </dev/null
 
 *)
 
+workbenchLocations = {"/Applications/Workbench.app/configuration/org.eclipse.osgi/bundles/13/1/.cp/MathematicaSourceVersioned/Head", "/Applications/Wolfram Workbench.app/configuration/org.eclipse.osgi/bundles/13/1/.cp/MathematicaSourceVersioned/Head"};
+
+AppendTo[$Path,
+  Replace[Select[workbenchLocations, DirectoryQ],
+    {{}    :> (Print["Wolfram Workbench not found"]; Abort[]),
+      {x_, ___} :> x}]];
 
 SetOptions["stdout", PageWidth -> Infinity];
 
