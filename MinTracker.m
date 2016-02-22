860d5fef5beae4905ef0b58e6fa26898d0d8cd17
Barry Wardell <barry.wardell@gmail.com>
2016-02-22 09:46:16 +0000

    MinTracker: Use Part instead of Map when creating DataTables
    It's much faster to use Part's functionality to pick apart lists rather
    than using Map.

--- MinTracker.m
+++ MinTracker.m
@@ -29,10 +29,10 @@ SimulationTools`MinTracker`BHCoordinates`ReadBHCoordinates[runName_String, track
   Table[SimulationTools`MinTracker`BHCoordinates`ReadBHCoordinates[runName, t], {t, trackers}];
 
 SimulationTools`MinTracker`BHCoordinates`ReadBHCoordinates[runName_String, tracker_Integer] :=
-  Module[{list, list2},
-    list = ReadColumnFile[runName, fileName[tracker], {2,3,4,5}];
-    list2 = Map[{#[[1]], {#[[2]], #[[3]], #[[4]]}} &, list];
-    Return[MakeDataTable[list2, {RunName -> runName}]]];
+ Module[{data},
+  data = ReadColumnFile[runName, fileName[tracker], {2,3,4,5}];
+  AddAttribute[ToDataTable[data[[All, 1]], data[[All, {2,3,4}]]], {RunName -> runName}]
+];
 
 SimulationTools`MinTracker`Trackers`ReadCoordinates[runName_String, trackers_List] :=
   Table[SimulationTools`MinTracker`Trackers`ReadCoordinates[runName, t], {t, trackers}];
