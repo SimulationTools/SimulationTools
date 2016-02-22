256ebc25a1083caf9793e945c25c4840a7acbfa7
Barry Wardell <barry.wardell@gmail.com>
2016-02-22 10:32:46 +0000

    Trackers: Add support for reading multiple trackers at once

--- Trackers.m
+++ Trackers.m
@@ -28,48 +28,72 @@ ReadTrackerInclination::usage = "ReadTrackerInclination[sim, tracker] gives the 
 
 Begin["`Private`"];
 
-trackerPattern = {_String, _Integer};
+trackerPattern = {_String, (_Integer|_List)};
 
-ReadTrackerCoordinates[run_String, tracker:{type_, i_}] :=
-  Symbol["SimulationTools`"<>type<>"`Trackers`ReadCoordinates"][run, i];
+ReadTrackerCoordinates[run_String, tracker:trackerPattern] :=
+  Symbol["SimulationTools`"<>tracker[[1]]<>"`Trackers`ReadCoordinates"][run, tracker[[2]]];
 
 ReadTrackerCoordinates[run_String, tracker1:trackerPattern, tracker2:trackerPattern] :=
   ReadTrackerCoordinates[run, tracker1] - ReadTrackerCoordinates[run, tracker2];
 
 ReadTrackerVelocity[run_String, tracker:trackerPattern] :=
-  NDerivative[1]/@ReadTrackerCoordinates[run, tracker];
+ Module[{coords},
+  coords = ReadTrackerCoordinates[run, tracker];
+  Map[NDerivative[1], coords, {ArrayDepth[coords]}]
+];
 
 ReadTrackerVelocity[run_String, tracker1:trackerPattern, tracker2:trackerPattern] :=
-  NDerivative[1]/@ReadTrackerCoordinates[run, tracker1, tracker2];
+ Module[{coords},
+  coords = ReadTrackerCoordinates[run, tracker1, tracker2];
+  Map[NDerivative[1], coords, {ArrayDepth[coords]}]
+];
 
 ReadTrackerRadius[run_String, tracker:trackerPattern] :=
-  Norm@ReadTrackerCoordinates[run, tracker];
+ Module[{coords},
+  coords = ReadTrackerCoordinates[run, tracker];
+  Map[Norm, coords, {ArrayDepth[coords]-1}]
+];
 
 ReadTrackerRadius[run_String, tracker1:trackerPattern, tracker2:trackerPattern] :=
-  Norm@ReadTrackerCoordinates[run, tracker1, tracker2];
+ Module[{coords},
+  coords = ReadTrackerCoordinates[run, tracker1, tracker2];
+  Map[Norm, coords, {ArrayDepth[coords]-1}]
+];
 
 xyToAzimuth[{x1_DataTable, y1_DataTable}] :=
   UnwrapPhaseVector[ArcTan@@RestrictedToCommonInterval[{x1,y1}]];
 
 ReadTrackerAzimuth[run_String, tracker:trackerPattern] :=
-  xyToAzimuth[ReadTrackerCoordinates[run, tracker][[{1,2}]]];
+ Module[{coords},
+  coords = ReadTrackerCoordinates[run, tracker];
+  Map[xyToAzimuth[#[[{1,2}]]]&, coords, {ArrayDepth[coords]-1}]
+];
 
 ReadTrackerAzimuth[run_String, tracker1:trackerPattern, tracker2:trackerPattern] :=
-  xyToAzimuth[(ReadTrackerCoordinates[run, tracker1] - ReadTrackerCoordinates[run, tracker2])[[{1,2}]]];
+ Module[{coords},
+  coords = ReadTrackerCoordinates[run, tracker1, tracker2];
+  Map[xyToAzimuth[#[[{1,2}]]]&, coords, {ArrayDepth[coords]-1}]
+];
+
+inclination[{x_DataTable, y_DataTable, z_DataTable}] :=
+ Module[{R},
+  R = {x,y,z};
+  (* In BHCoordinates, we avoided infinities at the origin:
+     MapData[If[Norm[#] > 10^-4, ArcCos[#[[3]]/Norm[#]], 0.0] &, rp]; *)
+  ArcCos[z/Norm[R]]
+];
 
 ReadTrackerInclination[run_String, tracker:trackerPattern] :=
-  Module[{R,x,y,z},
-    R = {x,y,z} = ReadTrackerCoordinates[run, tracker];
-    (* In BHCoordinates, we avoided infinities at the origin:
-       MapData[If[Norm[#] > 10^-4, ArcCos[#[[3]]/Norm[#]], 0.0] &, rp]; *)
-    ArcCos[z/Norm[R]]]; (* FIXME: shouldn't this be ArcCos[z/Norm[R]]? *)
+ Module[{coorda},
+  coords = ReadTrackerCoordinates[run, tracker];
+  Map[inclination, coords, {ArrayDepth[coords]-1}]
+];
 
 ReadTrackerInclination[run_String, tracker1:trackerPattern, tracker2:trackerPattern] :=
-  Module[{R,x,y,z},
-    R = {x,y,z} = ReadTrackerCoordinates[run, tracker1] - ReadTrackerCoordinates[run, tracker2];
-    (* In BHCoordinates, we avoided infinities at the origin:
-       MapData[If[Norm[#] > 10^-4, ArcCos[#[[3]]/Norm[#]], 0.0] &, rp]; *)
-    ArcCos[z]/Norm[R]];
+ Module[{coorda},
+  coords = ReadTrackerCoordinates[run, tracker1, tracker2];
+  Map[inclination, coords, {ArrayDepth[coords]-1}]
+];
 
 End[];
 EndPackage[];
