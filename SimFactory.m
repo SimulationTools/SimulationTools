cbe8f3743ee1a2a29da592edeeadc56a786df04e
Barry Wardell <barry.wardell@gmail.com>
2016-05-12 15:47:32 -0400

    SimFactory: Remove some Unix-specific paths

--- SimFactory.m
+++ SimFactory.m
@@ -42,17 +42,17 @@ getDataSubDir[output_String] :=
        Return[runName]];
 
     (* If this fails, look for the directory containing the parfile *)
-    parFiles = FileNames["*/*.par", {output}, 2];
+    parFiles = FileNames["*" <> $PathnameSeparator <> "*.par", {output}, 2];
     parFiles = Select[parFiles, !StringMatchQ[#, __~~"SIMFACTORY"~~__] &];
     If[Length[parFiles] === 0, Return[None]];
-    If[Length[parFiles] =!= 1, Error["Found more than one */*.par in " <> output]];
+    If[Length[parFiles] =!= 1, Error["Found more than one *" <> $PathnameSeparator <> "*.par in " <> output]];
     FileNameTake[parFiles[[1]], {-2}]
 ];
 
 SimulationTools`SimFactory`RunFiles`FindRunDirSegments[dir_String] :=
   Module[
     {restarts, merged, dataSubDir, dataDirs, segments},
-    restarts = Select[FileNames["output-*", dir], ! StringMatchQ[#, "*/output-*-*"] &];
+    restarts = Select[FileNames["output-*", dir], ! StringMatchQ[#, "*" <> $PathnameSeparator <> "output-*-*"] &];
     merged = FileNames["merged", dir];
     dataSubDir = getDataSubDir[restarts[[1]]];
     If[dataSubDir === None, Error["Unable to locate data directories in simulation "<>dir]];
@@ -66,13 +66,13 @@ SimulationTools`SimFactory`RunFiles`ReadCores[dir_, runName_] :=
     {files, file},
     files = Select[{FileNameJoin[{dir, "SIMFACTORY", "PROCS"}],
                     FileNameJoin[{dir, "PROCS"}],
-                    FileNameJoin[{dir, "SIMFACTORY/properties.ini"}]},
+                    FileNameJoin[{dir, "SIMFACTORY", "properties.ini"}]},
                    FileExistsQ];
     If[Length[files] === 0,
        Error[NoSimulationCoreCountAvailable, "Simulation core count not available in \""<>runName<>"\""]];
     file = Last[files];
     If[FileNameTake[file,-1] === "properties.ini",
-       ToExpression@IniVariable[FindRunSegments[runName][[-1]]<>"/../SIMFACTORY/properties.ini", "procs"],
+       ToExpression@IniVariable[FindRunSegments[runName][[-1]] <> $PathnameSeparator <> ".." <> $PathnameSeparator <> "SIMFACTORY" <> $PathnameSeparator <> "properties.ini", "procs"],
        ReadList[file, Number][[1]]]];
 
 (* TODO: refactor with above function *)
@@ -81,25 +81,25 @@ SimulationTools`SimFactory`RunFiles`ReadProcesses[dir_, runName_] :=
     {files, file},
     files = Select[{FileNameJoin[{dir, "SIMFACTORY", "PROCS"}],
                     FileNameJoin[{dir, "PROCS"}],
-                    FileNameJoin[{dir, "SIMFACTORY/properties.ini"}]},
+                    FileNameJoin[{dir, "SIMFACTORY", "properties.ini"}]},
                    FileExistsQ];
     If[Length[files] === 0,
        Error[NoSimulationCoreCountAvailable, "Simulation process count not available in \""<>runName<>"\""]];
     file = First[files];
     If[FileNameTake[file,-1] === "properties.ini",
-       ToExpression@IniVariable[FindRunSegments[runName][[1]]<>"/../SIMFACTORY/properties.ini", "numprocs"],
+       ToExpression@IniVariable[FindRunSegments[runName][[1]] <> $PathnameSeparator <> ".." <> $PathnameSeparator <> "SIMFACTORY" <> $PathnameSeparator <> "properties.ini", "numprocs"],
        ReadList[file, Number][[1]]]];
 
 SimulationTools`SimFactory`RunFiles`ReadSimulationMachine[dir_String, runName_String] :=
   ReadSimFactoryMetadata[runName, "machine"];
 
 ReadSimFactoryMetadata[simName_, key_] :=
-  IniVariable[FindRunSegments[simName][[1]]<>"/../SIMFACTORY/properties.ini", key];
+  IniVariable[FindRunSegments[simName][[1]] <> $PathnameSeparator <> ".." <> $PathnameSeparator <> "SIMFACTORY" <> $PathnameSeparator <> "properties.ini", key];
 
 
 ReadSimulationJobIdentifiers[sim_String] :=
  IniVariable[#, "jobid"] & /@ 
-  FindSimulationFiles[sim, "../SIMFACTORY/properties.ini"];
+  FindSimulationFiles[sim, ".." <> $PathnameSeparator <> "SIMFACTORY" <> $PathnameSeparator <> "properties.ini"];
 
 End[];
 
