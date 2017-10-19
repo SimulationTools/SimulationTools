9062cad3bcc2103701966858306af547aa71329b
Ian Hinder <ian.hinder@aei.mpg.de>
2017-10-19 19:13:03 +0200

    Grids.m: Support arbitrary number of levels in TimeRefinementFactors

--- Grids.m
+++ Grids.m
@@ -536,13 +536,15 @@ ReadCoarseTimeStep[runName_] :=
   ];
 
 TimeRefinementFactors[runName_String] :=
-  Module[{s1, s2, s3s, facs},
-    s1 = LookupParameter[runName, "Carpet::time_refinement_factors", "[1,2,4,8,16,32,64,128,256]"];
-    s2 = StringCases[s1, ("[" ~~ facs__ ~~ "]") -> facs][[1]];
-    s3s = StringSplit[s2, ","];
-    facs = Map[ToExpression, s3s];
-    Return[facs];
-  ];
+  Module[{s1, s2, s3s, facs, maxRLs},
+    s1 = LookupParameter[runName, "Carpet::time_refinement_factors", Automatic];
+
+    If[s1 === Automatic,
+      Table[2^rl, {rl, 0, ReadMaxRefinementLevels[runName] - 1}],
+      (* else *)
+      s2 = StringCases[s1, ("[" ~~ facs__ ~~ "]") -> facs][[1]];
+      s3s = StringSplit[s2, ","];
+      Map[ToExpression, s3s]]];
 
 TimeRefinementFactor[runName_String, level_] :=
   TimeRefinementFactors[runName][[level+1]];
